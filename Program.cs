8fa5eccae957449bfbed55a0996c92438db35642
Evangelia Bibasi <euaggeliampimpase@gmail.com>
2025-10-21 20:25:17 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 
 namespace Calculator
 {
@@ -74,4 +73,5 @@ namespace Calculator
             Console.ReadKey();
         }
     }
+
 }
